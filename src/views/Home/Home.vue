2230ea8bba751e8e641180cb2580c569552b7e4c
jeffy <jeffylab@outlook.com>
2018-05-05 18:50:19 +0800

    wanc

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -2,23 +2,25 @@
   <div class="home">
       <el-card class="box-card">
         <h3>个人信息</h3>
-        姓名：{{allData.onePact.userName}}
-        合同开始时间：{{allData.onePact.startTime}}
-        合同结束时间：{{allData.onePact.endTime}}
+        姓名：{{viewsChange(allData.onePact.userName)}} <p></p>
+        合同开始时间：{{viewsChange(allData.onePact.startTime)}} <p></p>
+        合同结束时间：{{viewsChange(allData.onePact.endTime)}} <p></p>
         <h3>薪资</h3>
-        基础薪资:{{allData.oneSalary.baseWage}}
-        补贴：{{allData.oneSalary.subsidy}}
+        基础薪资:{{viewsChange(allData.oneSalary.baseWage)}} <p></p>
+        补贴：{{viewsChange(allData.oneSalary.subsidy)}} <p></p>
         <h3>培训信息</h3>
-        培训名称：{{allData.oneTrain.userName}}
-        开始时间：{{allData.oneTrain.startTime}}
-        结束时间：{{allData.oneTrain.endTime}}
-
+        培训名称：{{viewsChange(allData.oneTrain.userName)}} <p></p>
+        开始时间：{{viewsChange(allData.oneTrain.startTime)}} <p></p>
+        结束时间：{{viewsChange(allData.oneTrain.endTime)}} <p></p>
         <h3>考勤</h3>
-        <p v-for="(item,index) in allData.userDay" :key="index">
-          <p>考勤信息：{{item.kq_date}}</p>
-          <p>工作日：{{item.work_days}}</p>
-          <p>出勤天数：{{item.really_days}}</p>
-        </p>
+        <div v-if="allData.userDay.length==0">
+          {{"暂无考勤"}}
+        </div>
+        <div v-if="allData.userDay>0" v-for="(item,index) in allData.userDay" :key="index">
+          <p>考勤信息：{{viewsChange(item.kq_date)}}</p>
+          <p>工作日：{{viewsChange(item.work_days)}}</p>
+          <p>出勤天数：{{viewsChange(item.really_days)}}</p>
+        </div>
       </el-card>
   </div>
 </template>
@@ -31,11 +33,29 @@ export default {
       allData: {}
     };
   },
+  methods:{
+    viewsChange(data){
+     return data?data:'暂无信息'
+    }
+  },
   created() {
     userData
       .getAllData({})
       .then(res => {
-        this.allData = res.data.data;
+        let list = res.data.data
+        if(!list.onePact){
+          list.onePact = {}
+        }
+        if(!list.oneSalary){
+          list.oneSalary = {}
+        }
+        if(!list.oneTrain){
+          list.oneTrain = {}
+        }
+        if(!list.oneUser){
+          list.oneUser = {}
+        }
+        this.allData = list;
       })
       .catch(err => {});
   }
